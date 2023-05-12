c09cf1ab8361839db7dd34858f039175b3daebdc
sedes <marianorivero97@gmail.com>
2023-05-12 19:19:03 -0300

    12/05

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton = findViewById<Button>(R.id.btn_open_activity)
+        boton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this@MainActivity,"texto",Toast.LENGTH_SHORT).show()
+        })
     }
 }
