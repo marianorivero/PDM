549304d0b6f0cc1e59d07f8a2243389a2edb7340
sedes <marianorivero97@gmail.com>
2023-06-02 19:38:34 -0300

    02-06-2023

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -9,10 +11,59 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("onCreate1","Metodo OnCreate1")
+
         setContentView(R.layout.activity_main)
         val boton = findViewById<Button>(R.id.btn_open_activity)
-        boton.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this@MainActivity,"texto",Toast.LENGTH_SHORT).show()
-        })
+        val miOnClick = MiOnClick()
+        boton.setOnClickListener(miOnClick)
+
+//        boton.setOnClickListener(View.OnClickListener {
+//            Toast.makeText(this@MainActivity,"texto de Ejemplo",Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+//            startActivity(intent)
+//        })
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("onStart1","Metodo OnStart2")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResume1","Metodo OnResume2")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause1","Metodo OnPause2")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStop1","Metodo OnStop2")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("onDestroy1","Metodo OnDestroy2")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("onRestart1","Metodo OnRestart1")
+    }
+}
+
+class MiOnClick:View.OnClickListener{
+    override fun onClick(p0: View?) {
+        Toast.makeText(p0?.context,"texto de Ejemplo",Toast.LENGTH_SHORT).show();
+        val intent = Intent(p0?.context, MainActivity2::class.java)
+        intent.putExtra("key","value cualquier valor")
+        p0?.context?.startActivity(intent)
+    }
+
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -2,10 +2,38 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val valor = intent.extras?.getString("key")
+        Log.i("tag", valor.toString())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("onStart2","Metodo OnStart2")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResume2","Metodo OnResume2")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause2","Metodo OnPause2")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStop2","Metodo OnStop2")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("onDestroy2","Metodo OnDestroy2")
     }
 }
