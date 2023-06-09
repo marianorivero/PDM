b15869903a41494a87333723ca9c9e331017385e
sedes <marianorivero97@gmail.com>
2023-06-09 19:20:39 -0300

    09-06-2023

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -2,15 +2,35 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var recyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         val valor = intent.extras?.getString("key")
         Log.i("tag", valor.toString())
+        
+        recyclerView = findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
+
+    private fun generarNombres() : List<String> {
+        val nameList = mutableListOf<String>()
+        for (i in 1..100){
+            nameList.add("Name $i")
+        }
+        return nameList
+    }
+    
 
     override fun onStart() {
         super.onStart()
@@ -37,3 +57,22 @@ class MainActivity2 : AppCompatActivity() {
         Log.i("onDestroy2","Metodo OnDestroy2")
     }
 }
+
+class UserAdapter (private val nameList : List<String>): RecyclerView.Adapter<UserAdapter.UserViewHolder>(){
+    class UserViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): UserViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(p0: UserViewHolder, p1: Int) {
+        TODO("Not yet implemented")
+    }
+}
+//implemetar la clase UserAdapter
